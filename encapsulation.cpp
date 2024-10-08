b194a66d9149ab0a5aa1bda5d1afa6ad3aea28ae
Sarowarprotik12 <145317761+Sarowarprotik12@users.noreply.github.com>
2024-10-08 23:58:22 +0600

    Add files via upload

---
+++ encapsulation.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class pr{
+private:
+
+int account_number;
+int balance;
+
+public:
+void setAccount_number(int a){
+    account_number=a;
+}
+
+int getAccount_number(){
+    return account_number;
+}
+
+
+void setBalance(int b){
+    balance=b;
+}
+
+int getBalance(){
+    return balance;
+}
+
+
+};
+
+
+
+
+
+int main(){
+
+pr r;
+r.setAccount_number(890);
+r.setBalance(20000);
+
+cout<<r.getAccount_number()<<endl;
+cout<<r.getBalance()<<endl;
+
+    return 0;
+}
